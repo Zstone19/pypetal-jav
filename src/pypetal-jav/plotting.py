8e631f2d8fbe7750d0abf3c66dd8da4aefc4b07d
Zstone19 <stone28@illinois.edu>
2023-03-31 18:31:03 -0500

    Fixing mpl params

--- src/pypetal-jav/plotting.py
+++ src/pypetal-jav/plotting.py
@@ -1,13 +1,12 @@
 import corner
 import javelin.lcmodel
 import matplotlib as mpl
-mpl.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import palettable
 from matplotlib.colors import ListedColormap
 
-
+mpl.use('Agg')
 mpl.rcParams['xtick.minor.visible'] = True
 mpl.rcParams['xtick.top'] = True
 mpl.rcParams['xtick.direction'] = 'in'
